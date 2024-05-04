d16968d3de7f8736f9d6d1837e583d07a9b9caee
RossaMania <raclettenberg@gmail.com>
2024-05-04 16:55:22 -0500

    feat: Update student scores output with letter grades.

--- StudentGrades/Program.cs
+++ StudentGrades/Program.cs
@@ -43,10 +43,10 @@ namespace StudentGrades
             decimal zahirahScore = (decimal) zahirahSum / currentAssignments;
             decimal jeongScore = (decimal) jeongSum / currentAssignments;
 
-            Console.WriteLine("Sophia: " + sophiaScore);
-            Console.WriteLine("Nicolas: " + nicolasScore);
-            Console.WriteLine("Zahirah: " + zahirahScore);
-            Console.WriteLine("Jeong: " + jeongScore);
+            Console.WriteLine("Sophia: " + sophiaScore + " A");
+            Console.WriteLine("Nicolas: " + nicolasScore + " B");
+            Console.WriteLine("Zahirah: " + zahirahScore + " B");
+            Console.WriteLine("Jeong: " + jeongScore + " A");
         }
     }
 }
