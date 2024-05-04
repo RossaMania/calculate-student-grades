85845778cd1e117834c1f34447fd8f05e9d9b2c3
RossaMania <raclettenberg@gmail.com>
2024-05-04 16:52:48 -0500

    feat: Cast the Sum vars as decimal types so the Score vars deliver a decimal result.

--- StudentGrades/obj/Debug/net8.0/StudentGrades.AssemblyInfo.cs
+++ StudentGrades/obj/Debug/net8.0/StudentGrades.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentGrades")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b03e97aff0a41cd5562c44d54db8972e723cd569")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5a68b5ab61e23b063b230246068a9d766c41dda")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentGrades")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentGrades")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
