f64d54eeef09e3c855d2f29107bfc9deeebe9a36
RossaMania <raclettenberg@gmail.com>
2024-05-09 19:26:57 -0500

    Update tests in test file.

--- StudentGrades.Tests/StudentGradesTests.cs
+++ StudentGrades.Tests/StudentGradesTests.cs
@@ -5,7 +5,7 @@ namespace StudentGrades.Tests
     public class StudentGradesTests
     {
         [Test]
-        public void CalculateSophiaGrade_ReturnsA()
+        public void CalculateSophiaGrade_ReturnsAWithExtraCredit()
         {
             // Arrange
             var sophia1 = 93;
@@ -13,91 +13,48 @@ namespace StudentGrades.Tests
             var sophia3 = 98;
             var sophia4 = 95;
             var sophia5 = 100;
+            var sophiaExtraCredit = 10; // Extra credit points
             var currentAssignments = 5;
             var expectedGrade = "A";
 
             // Act
-            var sophiaSum = sophia1 + sophia2 + sophia3 + sophia4 + sophia5;
-            var sophiaScore = (decimal)sophiaSum / currentAssignments;
+            var sophiaSum = sophia1 + sophia2 + sophia3 + sophia4 + sophia5 + sophiaExtraCredit;
+            var totalAssignments = currentAssignments + 1; // Including extra credit assignment
+            var sophiaScore = (decimal)sophiaSum / totalAssignments;
             var actualGrade = GetGrade(sophiaScore);
 
             // Assert
             Assert.AreEqual(expectedGrade, actualGrade);
         }
 
-        [Test]
-        public void CalculateNicolasGrade_ReturnsB()
-        {
-            // Arrange
-            var nicolas1 = 80;
-            var nicolas2 = 83;
-            var nicolas3 = 82;
-            var nicolas4 = 88;
-            var nicolas5 = 85;
-            var currentAssignments = 5;
-            var expectedGrade = "B";
-
-            // Act
-            var nicolasSum = nicolas1 + nicolas2 + nicolas3 + nicolas4 + nicolas5;
-            var nicolasScore = (decimal)nicolasSum / currentAssignments;
-            var actualGrade = GetGrade(nicolasScore);
-
-            // Assert
-            Assert.AreEqual(expectedGrade, actualGrade);
-        }
-
-        [Test]
-        public void CalculateZahirahGrade_ReturnsB()
-        {
-            // Arrange
-            var zahirah1 = 84;
-            var zahirah2 = 96;
-            var zahirah3 = 73;
-            var zahirah4 = 85;
-            var zahirah5 = 79;
-            var currentAssignments = 5;
-            var expectedGrade = "B";
-
-            // Act
-            var zahirahSum = zahirah1 + zahirah2 + zahirah3 + zahirah4 + zahirah5;
-            var zahirahScore = (decimal)zahirahSum / currentAssignments;
-            var actualGrade = GetGrade(zahirahScore);
-
-            // Assert
-            Assert.AreEqual(expectedGrade, actualGrade);
-        }
-
-        [Test]
-        public void CalculateJeongGrade_ReturnsA()
-        {
-            // Arrange
-            var jeong1 = 90;
-            var jeong2 = 92;
-            var jeong3 = 98;
-            var jeong4 = 100;
-            var jeong5 = 97;
-            var currentAssignments = 5;
-            var expectedGrade = "A";
-
-            // Act
-            var jeongSum = jeong1 + jeong2 + jeong3 + jeong4 + jeong5;
-            var jeongScore = (decimal)jeongSum / currentAssignments;
-            var actualGrade = GetGrade(jeongScore);
-
-            // Assert
-            Assert.AreEqual(expectedGrade, actualGrade);
-        }
+        // Add similar tests for other students with extra credit
 
         private string GetGrade(decimal score)
         {
-            if (score >= 90)
+            if (score >= 97)
+                return "A+";
+            else if (score >= 93)
                 return "A";
-            else if (score >= 80)
+            else if (score >= 90)
+                return "A-";
+            else if (score >= 87)
+                return "B+";
+            else if (score >= 83)
                 return "B";
-            else if (score >= 70)
+            else if (score >= 80)
+                return "B-";
+            else if (score >= 77)
+                return "C+";
+            else if (score >= 73)
                 return "C";
-            else if (score >= 60)
+            else if (score >= 70)
+                return "C-";
+            else if (score >= 67)
+                return "D+";
+            else if (score >= 63)
                 return "D";
+            else if (score >= 60)
+                return "D-";
             else
                 return "F";
         }
